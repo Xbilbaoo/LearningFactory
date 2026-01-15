f07961c7456bb500943c5566eb5491427864935f
XBilbao <x_bilbaozam@fpzornotzalh.eus>
2026-01-15 13:56:43 +0100

    added some mediaQueries

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'header-component',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  isMobile = false;
 
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private cd: ChangeDetectorRef
+  ) {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset, '(max-width: 767px)'])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+        this.cd.detectChanges();
+      });
+  }
 }
+
