2cb18f704289b16eb3c4e357e15dd8e1baafd5de
2AW3 User 8 <2AW3-8@yourdomain.com>
2026-01-21 08:35:03 +0100

    Aç

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Compra } from './components/compra/compra';
 import { Landing } from './components/landing/landing';
+import { Formulario } from "./components/formulario/formulario";
+import { SobreNosotros } from "./components/sobre-nosotros/sobre-nosotros";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Compra, Header],
+  imports: [RouterOutlet, Compra, Header, Landing, Formulario, SobreNosotros],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
