6371bb006d5164426b97ab9124ac378fef5fe184
2AW3 User 8 <2AW3-8@yourdomain.com>
2026-01-21 09:08:28 +0100

    minor changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Compra } from './components/compra/compra';
 import { Landing } from './components/landing/landing';
+import { Formulario } from "./components/formulario/formulario";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Compra, Header],
+  imports: [RouterOutlet, Compra, Header, Landing, Formulario, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
