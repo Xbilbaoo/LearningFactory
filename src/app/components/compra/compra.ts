187103f78c6f4fa5543bdb19b73e7826d08b31f8
IzaAne <a_izaguirrelor@fpzornotzalh.eus>
2026-01-23 12:14:19 +0100

    imagenes(ts)

--- src/app/components/compra/compra.ts
+++ src/app/components/compra/compra.ts
@@ -8,12 +8,32 @@ import { Carousel } from '../carousel/carousel';
   templateUrl: './compra.html',
   styleUrl: './compra.scss',
 })
+
 export class Compra {
 imagenActual: string = '../../../../IMG/web_basura_orilla_4f151f7a30.png';
+imagenAnterior: string = '../../../../IMG/web_basura_orilla_4f151f7a30.png';
+vision = true;
 
 cambiarImagen(img : string) {
   this.imagenActual=img;
+  if(this.vision){
+    this.vision = false
+  }
+}
 
+imagenb(img : string){
+  this.imagenAnterior = this.imagenActual;
+  this.vision =true
+
+  if(this.vision){
+    this.imagenActual=img;
+  }
+}
+
+imagena(){
+  if(this.vision){
+    this.imagenActual=this.imagenAnterior;
+  }
 }
 
 }
