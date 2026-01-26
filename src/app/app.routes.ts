66d9f48820ff1e52eedfaae997bcc4cdf6edc857
2AW3 User 8 <2AW3-8@yourdomain.com>
2026-01-26 09:29:40 +0100

    Comentarios varios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { Landing } from './components/landing/landing';
 import { SobreNosotros } from './components/sobre-nosotros/sobre-nosotros';
 import { Compra } from './components/compra/compra';
+import { Formulario } from './components/formulario/formulario';
 
 export const routes: Routes = [ 
 
     { path: '', redirectTo: '/Home', pathMatch: 'full' },
     { path: 'Home', component: Landing },
     { path: 'About', component: SobreNosotros },
-    { path: 'Buy', component: Compra }
+    { path: 'Buy', component: Compra },
+    { path: 'Form', component: Formulario}
 
 ];
